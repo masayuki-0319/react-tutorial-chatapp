d4a308e237177dc547652c472c8ca69191dbb84c
将之大西 <omccello@gmail.com>
2021-06-08 23:16:53 +0900

    finish step13

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import SignIn from './Signin';
+
+function App() {
+  return (
+    <div>
+      <SignIn />
+    </div>
+  );
+}
+
+export default App;
