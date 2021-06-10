2ccddad415ddedeef3646fd802b795b462241f31
将之大西 <omccello@gmail.com>
2021-06-10 23:36:07 +0900

    finish step 23

--- src/components/MessageInputField.tsx
+++ src/components/MessageInputField.tsx
@@ -1,9 +1,18 @@
 import { VFC } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    gridRow: 2,
+  },
+});
 
 type Props = {
   name: string;
 };
 
 export const MessageInputField: VFC<Props> = ({ name }) => {
-  return <div>MessageInputField</div>;
+  const classes = useStyles();
+
+  return <div className={classes.root}>MessageInputField</div>;
 };
--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -1,5 +1,14 @@
+import { makeStyles } from '@material-ui/core';
 import { VFC } from 'react';
 
+const useStyles = makeStyles({
+  root: {
+    gridRow: 1,
+  },
+});
+
 export const MessageList: VFC = () => {
-  return <div>MessageList</div>;
+  const classes = useStyles();
+
+  return <div className={classes.root}>MessageList</div>;
 };
